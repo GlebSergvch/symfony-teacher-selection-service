d94f01411149687a386ababd30e399c421b25ebc
Gleb <shukhleev.talan@gmail.com>
2023-07-01 17:55:38 +0500

    hw1 added migration for entities

---
+++ migrations/Version20230613150927.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+class Version20230613150927 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql('CREATE TABLE "user" (id BIGSERIAL NOT NULL, login VARCHAR(32) NOT NULL, role VARCHAR(32) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "user_profile" (user_id BIGINT DEFAULT NULL, firstname VARCHAR(128), middlename VARCHAR(128), lastname VARCHAR(128), gender VARCHAR(32), PRIMARY KEY(user_id))');
+        $this->addSql('CREATE TABLE "skill" (id BIGSERIAL NOT NULL, name VARCHAR(128) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, created_by BIGINT DEFAULT NULL, updated_by BIGINT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE "group" (id BIGSERIAL NOT NULL, name VARCHAR(128) NOT NULL, minimum_size INT NOT NULL, maximum_size INT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, created_by BIGINT DEFAULT NULL, updated_by BIGINT DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE student_group (student_id BIGINT DEFAULT NULL, group_id BIGINT DEFAULT NULL, PRIMARY KEY(student_id, group_id))');
+        $this->addSql('CREATE TABLE teacher_skill (teacher_id BIGINT DEFAULT NULL, skill_id BIGINT DEFAULT NULL, PRIMARY KEY(teacher_id, skill_id))');
+
+        $this->addSql('ALTER TABLE user_profile ADD CONSTRAINT user_profile__user_id__fk FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+
+        $this->addSql('CREATE INDEX student_group__student_id__idx ON student_group (student_id)');
+        $this->addSql('CREATE INDEX student_group__group_id__idx ON student_group (group_id)');
+        $this->addSql('CREATE UNIQUE INDEX student_group__student_id__group_id__uniq ON student_group (student_id, group_id)');
+        $this->addSql('ALTER TABLE student_group ADD CONSTRAINT student_group__student_id__fk FOREIGN KEY (student_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE student_group ADD CONSTRAINT student_group__group_id__fk FOREIGN KEY (group_id) REFERENCES "group" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+
+        $this->addSql('CREATE INDEX teacher_skill__teacher_id__idx ON teacher_skill (teacher_id)');
+        $this->addSql('CREATE INDEX teacher_skill__skill_id__idx ON teacher_skill (skill_id)');
+        $this->addSql('CREATE UNIQUE INDEX teacher_skill__teacher_id__skill_id__uniq ON teacher_skill (teacher_id, skill_id)');
+        $this->addSql('ALTER TABLE teacher_skill ADD CONSTRAINT teacher_skill__teacher_id__fk FOREIGN KEY (teacher_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE teacher_skill ADD CONSTRAINT teacher_skill__skill_id__fk FOREIGN KEY (skill_id) REFERENCES "skill" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('DROP TABLE teacher_skill');
+        $this->addSql('DROP TABLE student_group');
+        $this->addSql('DROP TABLE "group"');
+        $this->addSql('DROP TABLE skill');
+        $this->addSql('DROP TABLE "user"');
+    }
+}

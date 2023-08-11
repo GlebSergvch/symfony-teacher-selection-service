9644108bcc9d77884bcfc82fd7920969a29a39dc
Gleb <shukhleev.talan@gmail.com>
2023-08-11 14:40:14 +0500

    hw2 added manager`s methods

--- src/Manager/SkillManager.php
+++ src/Manager/SkillManager.php
@@ -3,6 +3,7 @@
 namespace App\Manager;
 
 use App\Entity\Skill;
+use App\Repository\SkillRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class SkillManager
@@ -12,6 +13,10 @@ class SkillManager
     }
 
 
+    /**
+     * @param string $skillName
+     * @return Skill
+     */
     public function create(string $skillName): Skill
     {
         $skill = new Skill();
@@ -24,16 +29,66 @@ class SkillManager
         return $skill;
     }
 
+    /**
+     * @param string $skillName
+     * @return int
+     */
+    public function saveSkill(string $skillName): int
+    {
+        $skill = new Skill();
+        $skill->setName($skillName);
+        $skill->setCreatedAt();
+        $skill->setUpdatedAt();
+        $this->entityManager->persist($skill);
+        $this->entityManager->flush();
+
+        return $skill->getId();
+    }
+
+    /**
+     * @param int $page
+     * @param int $perPage
+     * @return Skill[]
+     */
+    public function getSkills(int $page, int $perPage): array
+    {
+        /** @var SkillRepository $skillRepository */
+        $skillRepository = $this->entityManager->getRepository(Skill::class);
+        return $skillRepository->getSkills($page, $perPage);
+    }
+
+    /**
+     * @param int $id
+     * @return Skill|object|null
+     */
+    public function getSkillById(int $id)
+    {
+        $skillRepository = $this->entityManager->getRepository(Skill::class);
+        return $skillRepository->findOneBy(['id' => $id]);
+    }
+
+    /**
+     * @param string $name
+     * @return Skill[]
+     */
     public function findSkillByName(string $name): array
     {
         return $this->entityManager->getRepository(Skill::class)->findBy(['name' => $name]);
     }
 
+    /**
+     * @param int $id
+     * @return Skill[]
+     */
     public function findSkillById(int $id): array
     {
         return $this->entityManager->getRepository(Skill::class)->findBy(['id' => $id]);
     }
 
+    /**
+     * @param string $skillName
+     * @return Skill
+     */
     public function findOrCreateSkill(string $skillName): Skill
     {
         $skill = $this->entityManager->getRepository(Skill::class)->findOneBy(['name' => $skillName]);
@@ -42,5 +97,44 @@ class SkillManager
         }
 
         return $skill;
+    }
+
+    /**
+     * @param int $skillId
+     * @param string $name
+     * @return bool
+     */
+    public function updateSkill(int $skillId, string $name): bool
+    {
+        /** @var SkillRepository $skillRepository */
+        $skillRepository = $this->entityManager->getRepository(Skill::class);
+        /** @var Skill $skill */
+        $skill = $skillRepository->find($skillId);
+        if ($skill === null) {
+            return false;
+        }
+        $skill->setName($name);
+        $this->entityManager->flush();
+
+        return true;
+    }
+
+    /**
+     * @param int $skillId
+     * @return bool
+     */
+    public function deleteSkill(int $skillId): bool
+    {
+        /** @var SkillRepository $skillRepository */
+        $skillRepository = $this->entityManager->getRepository(Skill::class);
+        /** @var Skill $skill */
+        $skill = $skillRepository->find($skillId);
+        if ($skill === null) {
+            return false;
+        }
+        $this->entityManager->remove($skill);
+        $this->entityManager->flush();
+
+        return true;
     }
 }
--- src/Manager/TeacherSkillManager.php
+++ src/Manager/TeacherSkillManager.php
@@ -5,6 +5,7 @@ namespace App\Manager;
 use App\Entity\Skill;
 use App\Entity\TeacherSkill;
 use App\Entity\User;
+use App\Repository\TeacherSkillRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 class TeacherSkillManager
@@ -13,6 +14,11 @@ class TeacherSkillManager
     {
     }
 
+    /**
+     * @param User $teacher
+     * @param Skill $skill
+     * @return TeacherSkill
+     */
     public function create(User $teacher, Skill $skill): TeacherSkill
     {
         $teacherSkill = new TeacherSkill();
@@ -24,6 +30,55 @@ class TeacherSkillManager
         $this->entityManager->flush();
 
         return $teacherSkill;
+    }
+
+    /**
+     * @param User $user
+     * @param Skill $skill
+     * @return TeacherSkill
+     */
+    public function addTeacherSkill(User $user, Skill $skill)
+    {
+        $teacherSkill = new TeacherSkill();
+        $teacherSkill->setTeacher($user);
+        $teacherSkill->setSkill($skill);
+        $teacherSkill->setUpdatedAt();
+        $teacherSkill->setCreatedAt();
+
+        $this->entityManager->persist($teacherSkill);
+        $this->entityManager->flush();
+
+        return $teacherSkill;
+    }
+
+    /**
+     * @return TeacherSkill[]
+     */
+    public function getTeacherSkill(int $page, int $perPage): array
+    {
+        /** @var TeacherSkillRepository $teacherSkillRepository */
+        $teacherSkillRepository = $this->entityManager->getRepository(TeacherSkill::class);
+        return $teacherSkillRepository->getTeacherSkill($page, $perPage);
+    }
+
+    /**
+     * @param int $teacherId
+     * @param int $skillId
+     * @return bool
+     */
+    public function deleteTeacherSkill(int $teacherId, int $skillId)
+    {
+        /** @var TeacherSkillRepository $teacherSkillRepository */
+        $teacherSkillRepository = $this->entityManager->getRepository(TeacherSkill::class);
+        /** @var TeacherSkill $teacherSkill */
+        $teacherSkill = $teacherSkillRepository->findOneBy(['teacher_id' => $teacherId, 'skill_id' => $skillId]);
+        if ($teacherSkill === null) {
+            return false;
+        }
+        $this->entityManager->remove($teacherSkill);
+        $this->entityManager->flush();
+
+        return true;
     }
 
 }

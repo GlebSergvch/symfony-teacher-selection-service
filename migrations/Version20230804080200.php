c70880029f1599b0357b42ca1057dcaace6d7c95
Gleb <shukhleev.talan@gmail.com>
2023-08-14 16:16:42 +0500

    hw2 fixed pr

--- migrations/Version20230804080200.php
+++ migrations/Version20230804080200.php
@@ -19,27 +19,7 @@ final class Version20230804080200 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $logins = $this->connection->fetchAllKeyValue('SELECT id, login FROM "user" ORDER BY id');
-
-        $distinct = array_unique($logins);
-        $duplicates = array_diff_key($logins, $distinct);
-
-        foreach ($duplicates as $id => $login) {
-            $newUserLogin = $this->createUserLogin();
-            $this->addSql("UPDATE \"user\" SET login = '$newUserLogin' WHERE id = '$id'");
-        }
-
         $this->addSql('CREATE UNIQUE INDEX user__login__uniq__idx ON "user" (login)');
-    }
-
-    private function createUserLogin() {
-        $newUserLogin = 'user' . rand(1, 10000);
-        $userLoginExists = $this->connection->fetchOne("SELECT EXISTS (SELECT * FROM \"user\" WHERE login = '$newUserLogin')");
-        if ($userLoginExists) {
-            $this->createUserLogin();
-        }
-
-        return $newUserLogin;
     }
 
     public function down(Schema $schema): void
